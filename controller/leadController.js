2a8469b1639bc28a4219d240900cf0565a470864
unknown <rekha.kumari1928@gmail.com>
2025-05-27 12:21:07 +0530

    style: edit team, project, and task

--- controller/leadController.js
+++ controller/leadController.js
@@ -6,93 +6,92 @@ const mongoose = require("mongoose");
 
 const addNewLead = asyncHandler(async (req, res, next) => {
   try {
-        const lead = new Lead(req.body)
-    const saveLead = await lead.save()
-    const populatedLead = await Lead.findById(saveLead._id).populate("salesAgent");
-    res.status(201).json({message:"New lead created.", populatedLead})
-       
-    } catch (error) {
-        res.status(500).json({error: "Failed to create new lead.", error})
-    }
-});
-
-
-//get all lead
-
-const findLeadsWithFilters = asyncHandler (async (req, res) => {
-    const { salesAgent, status, tags, source , prioritySort, dateSort } = req.query;
-   
- 
-    const filter = {};
-    if (salesAgent) {
-      filter.salesAgent = salesAgent;
-    }
-
-    if (status) {
-      filter.status = status;
-    }
-
-     if (tags) filter.tags = { $in: tags.split(",") };
-
-    if (source) {
-      filter.source = source;
-    }
-    let sortOptions = {}
-
-const priorityOrder={
-    Low:1,
-    Medium:2,
-    High:3
-}
-
-
-//priority sorting 
-if(prioritySort === "Low-High"){
-    sortOptions.priority = 1
-}else if(prioritySort === "High-Low"){
-    sortOptions.priority = -1
-}
-
-//date sorting
-if(dateSort){
-    if(dateSort === "Newest-Oldest"){
-        sortOptions.createdAt = -1
-    }else if(dateSort === "Oldest-Newest"){
-        sortOptions.createdAt = 1;
-    }
-}
-
-  try {  
-    const allLeads = await Lead.find(filter).sort(sortOptions).populate('salesAgent');
-
-    if(prioritySort){
-            allLeads.sort((a, b) => {
-                const priorityA = priorityOrder[a.priority] || 0; // Convert to numeric priority
-                const priorityB = priorityOrder[b.priority] || 0; // Convert to numeric priority
-                return prioritySort === "Low-High"
-                  ? priorityA - priorityB
-                  : priorityB - priorityA;
-              })
-        }
-    res.status(200).json(allLeads);
+    const lead = new Lead(req.body);
+    const saveLead = await lead.save();
+    const populatedLead = await Lead.findById(saveLead._id).populate(
+      "salesAgent"
+    );
+    res.status(201).json({ message: "New lead created.", populatedLead });
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Failed to create new lead.", error });
   }
 });
 
+//get all lead
 
+const findLeadsWithFilters = asyncHandler(async (req, res) => {
+  const { salesAgent, status, tags, source, prioritySort, dateSort } =
+    req.query;
+
+  const filter = {};
+  if (salesAgent) {
+    filter.salesAgent = salesAgent;
+  }
+
+  if (status) {
+    filter.status = status;
+  }
+
+  if (tags) filter.tags = { $in: tags.split(",") };
+
+  if (source) {
+    filter.source = source;
+  }
+  let sortOptions = {};
+
+  const priorityOrder = {
+    Low: 1,
+    Medium: 2,
+    High: 3,
+  };
+
+  //priority sorting
+  if (prioritySort === "Low-High") {
+    sortOptions.priority = 1;
+  } else if (prioritySort === "High-Low") {
+    sortOptions.priority = -1;
+  }
+
+  //date sorting
+  if (dateSort) {
+    if (dateSort === "Newest-Oldest") {
+      sortOptions.createdAt = -1;
+    } else if (dateSort === "Oldest-Newest") {
+      sortOptions.createdAt = 1;
+    }
+  }
+
+  try {
+    const allLeads = await Lead.find(filter)
+      .sort(sortOptions)
+      .populate("salesAgent");
+
+    if (prioritySort) {
+      allLeads.sort((a, b) => {
+        const priorityA = priorityOrder[a.priority] || 0; // Convert to numeric priority
+        const priorityB = priorityOrder[b.priority] || 0; // Convert to numeric priority
+        return prioritySort === "Low-High"
+          ? priorityA - priorityB
+          : priorityB - priorityA;
+      });
+    }
+    res.status(200).json(allLeads);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 //update lead
 
-const updateLeadById = asyncHandler( async (req, res) => {
+const updateLeadById = asyncHandler(async (req, res) => {
   try {
     const leadId = req.params.leadId;
     const dataToUpdate = req.body;
 
     if (!mongoose.Types.ObjectId.isValid(leadId)) {
       return res.status(400).json({ error: "Invalid Lead Id" });
-    } 
-   
+    }
+
     if (dataToUpdate.status === "Closed" && !dataToUpdate.closedAt) {
       dataToUpdate.closedAt = new Date();
     }
@@ -114,28 +113,63 @@ const updateLeadById = asyncHandler( async (req, res) => {
   }
 });
 
-//findById
-async function findLeadById(leadId) {
+
+//grouped lead
+async function getAllLeads() {
   try {
-    const lead = await Lead.findById(leadId).populate("salesAgent");
-    return lead;
+    const leads = await Lead.find().populate("salesAgent");
+    return leads;
   } catch (error) {
-    console.log("An error occured while finding lead.", error);
+    console.log("Error occured while loading leads.", error);
   }
 }
-
-const leadFindById = asyncHandler(async (req, res, next) => {
+const groupedLeadBy = asyncHandler(async (req, res, next) => {
   try {
-    const lead = await findLeadById(req.params.leadId);
-    if (lead) {
-      res.status(200).json({ lead: lead });
+    const leads = await getAllLeads();
+    const groupBy = (keys) => (array) =>
+      array.reduce((objectsByKeyValue, obj) => {
+        const value = keys.map((key) => obj[key]).join("-");
+        objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+        return objectsByKeyValue;
+      }, {});
+
+    const groupByBrand = groupBy(["status"]);
+
+    if (groupByBrand(leads)) {
+      res.status(201).json({
+        message: "Lead groupedBy is created as: ",
+        leadsByStatus: groupByBrand(leads),
+      });
     } else {
       res.status(404).json({ error: "Lead not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch lead.", error: error });
+    }}
+   catch (error) {
+    res.status(500).json({ error: "Failed to fetch leads", error: error });
   }
 });
+
+//findById
+// async function findLeadById(leadId) {
+//   try {
+//     const lead = await Lead.findById(leadId).populate("salesAgent");
+//     return lead;
+//   } catch (error) {
+//     console.log("An error occured while finding lead.", error);
+//   }
+// }
+
+// const leadFindById = asyncHandler(async (req, res, next) => {
+//   try {
+//     const lead = await findLeadById(req.params.leadId);
+//     if (lead) {
+//       res.status(200).json({ lead: lead });
+//     } else {
+//       res.status(404).json({ error: "Lead not found" });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ error: "Failed to fetch lead.", error: error });
+//   }
+// });
 
 //delete lead
 
@@ -223,9 +257,10 @@ const getAllComment = asyncHandler(async (req, res, next) => {
 module.exports = {
   addNewLead,
   findLeadsWithFilters,
+  groupedLeadBy,
   updateLeadById,
-  leadFindById,
+  // leadFindById,
   deleteLeadById,
   addComment,
-  getAllComment  
+  getAllComment,
 };
--- routes/leadRoute.js
+++ routes/leadRoute.js
@@ -1,25 +1,36 @@
-const express = require('express')
+const express = require("express");
 
 //lead router
-const router = express.Router()
+const router = express.Router();
 
 //import routes functions for lead
-const  { addNewLead,  findLeadsWithFilters, updateLeadById, leadFindById, deleteLeadById ,addComment, getAllComment} = require('../controller/leadController.js')
+const {
+  addNewLead,
+  findLeadsWithFilters,
+  updateLeadById,
+//   leadFindById,
+  groupedLeadBy,
+  deleteLeadById,
+  addComment,
+  getAllComment,
+} = require("../controller/leadController.js");
 
-const authMiddleware = require('../middleware/authMiddleware.js')
-
+const authMiddleware = require("../middleware/authMiddleware.js");
 
 //lead route
-router.post("/", authMiddleware, addNewLead)
-router.get("/",authMiddleware, findLeadsWithFilters)
-router.patch("/:leadId",authMiddleware, updateLeadById)
-router.get("/:leadId",authMiddleware, leadFindById)
-router.delete("/:leadId",authMiddleware, deleteLeadById)
+router.post("/", authMiddleware, addNewLead);
+router.get("/", authMiddleware, findLeadsWithFilters);
+router.patch("/:leadId", authMiddleware, updateLeadById);
+// router.get("/:leadId", authMiddleware, leadFindById);
+router.get("/grouped", authMiddleware, groupedLeadBy);
+
+router.delete("/:leadId", authMiddleware, deleteLeadById);
+
 //comment route
-router.post("/comments",authMiddleware, addComment)
-router.get("/comments",authMiddleware, getAllComment)
+router.post("/comments", authMiddleware, addComment);
+router.get("/comments", authMiddleware, getAllComment);
 
-router.post("/:leadId/comments",authMiddleware, addComment)
-router.get("/:leadId/comments",authMiddleware, getAllComment)
+router.post("/:leadId/comments", authMiddleware, addComment);
+router.get("/:leadId/comments", authMiddleware, getAllComment);
 
-module.exports = router
+module.exports = router;
